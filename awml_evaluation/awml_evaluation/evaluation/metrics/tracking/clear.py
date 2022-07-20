9c5a12e476b48a0c86953292b64709cdd79d8f6b
Kotaro Uetake <60615504+ktro2828@users.noreply.github.com>
2022-07-20 14:32:25 +0900

    fix(object assignment): fix problem multi estimated objects are assigned to 1 GT (#120)
    * fix(AP): fix first value of FP to 1.0
    
    * refactor(AP): add method to interpolate precision and recall
    
    * refactor(AP): refactoring TP/FP calculation
    
    * feat(PR-curve): add function to plot PR-curve
    
    * fix(PR-curve): fix undefined iou threshold
    
    * test(AP): update unittest for AP to check TP/FP and precision/recall
    
    * test(util): add class for differences of object
    
    * reafactor(AP): make calculation of TP/FP faster with numpy
    
    * fix(object result): fix the way of assignment ground truth for each estimated object
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * feat(matching): add consideration of matching mode and threshold in addition to confidence
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * fix(ap): fix return invalid FP value
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * fix(clear): fix count invalid id switch and tp score
    
    * fix(object result): fix object result to matching best matching score
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * test: update unittest for object result
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * refactor(object result): refactoring input of object result
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * fix(ap): remove non-required arguments in get_precision_recall_list()
    
    * fix(object result): fix to match objects priolitizing best matching score
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- awml_evaluation/awml_evaluation/evaluation/metrics/tracking/clear.py
+++ awml_evaluation/awml_evaluation/evaluation/metrics/tracking/clear.py
@@ -176,9 +176,6 @@ class CLEAR(_TrackingMetricsBase):
         num_id_switch: int = 0
         tp_matching_score: float = 0.0
 
-        # Assign previous results if same matching pair has
-        has_same_tp: bool = False
-        is_id_switched: bool = False
         for cur_obj_result in cur_object_results:
             # Assign previous results if same matching pair has
             matching_threshold_: float = get_label_threshold(
@@ -186,34 +183,33 @@ class CLEAR(_TrackingMetricsBase):
                 target_labels=self.target_labels,
                 threshold_list=self.matching_threshold_list,
             )
-            has_same_tp: bool = False
+            is_same_match: bool = False
             is_id_switched: bool = False
             for prev_obj_result in prev_object_results:
-                is_id_switched: bool = self._is_id_switched(
-                    cur_obj_result,
-                    prev_obj_result,
-                )
-                if is_id_switched:
-                    break
-                is_same_result: bool = self._is_same_result(
-                    cur_obj_result,
-                    prev_obj_result,
-                )
-                if is_same_result is False:
-                    continue
-
                 is_tp_prev: bool = prev_obj_result.is_result_correct(
                     self.matching_mode,
                     matching_threshold_,
                 )
+                # is_tp_cur: bool = cur_obj_result.is_result_correct(
+                #     self.matching_mode,
+                #     matching_threshold_,
+                # )
+                # if is_tp_prev is False or is_tp_cur is False:
+                if not is_tp_prev:
+                    continue
 
-                has_same_tp = is_same_result * is_tp_prev
-                if has_same_tp:
-                    # NOTE: The One GT can have multi matching estimated object
-                    tp += self.tp_metrics.get_value(cur_obj_result)
-                    tp_matching_score += cur_obj_result.get_matching(self.matching_mode).value
+                is_id_switched = self._is_id_switched(cur_obj_result, prev_obj_result)
+                if is_id_switched:
                     break
-            if has_same_tp:
+
+                is_same_match: bool = self._is_same_match(cur_obj_result, prev_obj_result)
+                if is_same_match:
+                    # NOTE: If current/previous has same matching pair and both current/previous is TP,
+                    #       previous TP score is assigned.
+                    tp += self.tp_metrics.get_value(prev_obj_result)
+                    tp_matching_score += prev_obj_result.get_matching(self.matching_mode).value
+                    break
+            if is_same_match:
                 continue
 
             # If there is no same matching pair with previous results
@@ -236,7 +232,7 @@ class CLEAR(_TrackingMetricsBase):
         cur_object_result: DynamicObjectWithPerceptionResult,
         prev_object_result: DynamicObjectWithPerceptionResult,
     ) -> bool:
-        """Check whether current and previous object result have switched ID.
+        """Check whether current and previous object result have switched ID for TP pairs.
 
         NOTE:
             There is a case the label is not same inspite of the same ID is given.
@@ -249,19 +245,26 @@ class CLEAR(_TrackingMetricsBase):
         Returns:
             bool: Return True if ID switched.
         """
+        # current GT = None -> FP
         if (
             cur_object_result.ground_truth_object is None
             or prev_object_result.ground_truth_object is None
         ):
             return False
 
+        # 1. Check whether current/previous estimated objects has same ID.
+        # NOTE: There is a case current/previous estimated objects has same ID,
+        #       but different label(Checked by 2.)
         has_same_estimated_id: bool = (
             cur_object_result.estimated_object.uuid == prev_object_result.estimated_object.uuid
         )
+        # 2. Check whether current/previous estimated objects has same label.
         has_same_estimated_label: bool = (
             cur_object_result.estimated_object.semantic_label
             == prev_object_result.estimated_object.semantic_label
         )
+        # 3. Check whether current/previous GT has same ID.
+        # NOTE: There is no case GT has same ID, but different label. (Like 1.)
         has_same_ground_truth_id: bool = (
             cur_object_result.ground_truth_object.uuid
             == prev_object_result.ground_truth_object.uuid
@@ -274,11 +277,12 @@ class CLEAR(_TrackingMetricsBase):
         return False
 
     @staticmethod
-    def _is_same_result(
+    def _is_same_match(
         cur_object_result: DynamicObjectWithPerceptionResult,
         prev_object_result: DynamicObjectWithPerceptionResult,
     ) -> bool:
         """Check whether current and previous object result have same matching pair.
+        When previous or current GT is None(=FP), return False regardless the ID of estimated.
 
         Args:
             cur_object_result (DynamicObjectWithPerceptionResult): An object result at current frame.
---
+++ awml_evaluation/test/evaluation/result/test_object_result.py
@@ -0,0 +1,85 @@
+from test.util.dummy_object import make_dummy_data
+from test.util.object_diff import DiffTranslation
+from typing import List
+from typing import Optional
+from typing import Tuple
+import unittest
+
+from awml_evaluation.common.object import DynamicObject
+from awml_evaluation.evaluation.result.object_result import DynamicObjectWithPerceptionResult
+from awml_evaluation.evaluation.result.object_result import get_object_results
+from awml_evaluation.util.debug import get_objects_with_difference
+
+
+class TestObjectResult(unittest.TestCase):
+    def setUp(self) -> None:
+        self.dummy_estimated_objects: List[DynamicObject] = []
+        self.dummy_ground_truth_objects: List[DynamicObject] = []
+        self.dummy_estimated_objects, self.dummy_ground_truth_objects = make_dummy_data()
+
+    def test_get_object_results(self):
+        """[summary]
+        Test matching estimated objects and ground truth objects.
+
+        test patterns:
+            NOTE:
+                - The estimations & GTs are following (number represents the index)
+                        Estimation = 3
+                            (0): CAR, (1): BICYCLE, (2): CAR
+                        GT = 4
+                            (0): CAR, (1): BICYCLE, (2): PEDESTRIAN, (3): MOTORBIKE
+        """
+        patterns: List[Tuple[DiffTranslation, List[Tuple[int, Optional[int]]]]] = [
+            # (1)
+            # (Est[0], GT[0]), (Est[1], GT[1]), (Est[0], None)
+            (
+                DiffTranslation((0.0, 0.0, 0.0), (0.0, 0.0, 0.0)),
+                [(0, 0), (1, 1), (2, None)],
+            ),
+            # (2)
+            # (Est[0], None), (Est[1], GT[1]), (Est[0], GT[0])
+            (
+                DiffTranslation((0.0, 0.0, 0.0), (-2.0, 0.0, 0.0)),
+                [(0, None), (1, 1), (2, 0)],
+            ),
+        ]
+        for n, (diff_trans, ans_pair_index) in enumerate(patterns):
+            with self.subTest(f"Test matching objects: {n + 1}"):
+                estimated_objects: List[DynamicObject] = get_objects_with_difference(
+                    ground_truth_objects=self.dummy_estimated_objects,
+                    diff_distance=diff_trans.diff_estimated,
+                    diff_yaw=0.0,
+                )
+                ground_truth_objects: List[DynamicObject] = get_objects_with_difference(
+                    ground_truth_objects=self.dummy_ground_truth_objects,
+                    diff_distance=diff_trans.diff_ground_truth,
+                    diff_yaw=0.0,
+                )
+
+                object_results: List[DynamicObjectWithPerceptionResult] = get_object_results(
+                    estimated_objects=estimated_objects,
+                    ground_truth_objects=ground_truth_objects,
+                )
+
+                for i, object_result_ in enumerate(object_results):
+                    self.assertIn(
+                        object_result_.estimated_object,
+                        estimated_objects,
+                        f"Unexpected estimated object at {i}",
+                    )
+                    estimated_object_index: int = estimated_objects.index(
+                        object_result_.estimated_object
+                    )
+                    gt_idx = ans_pair_index[estimated_object_index][1]
+                    if gt_idx is not None:
+                        self.assertEqual(
+                            object_result_.ground_truth_object,
+                            ground_truth_objects[gt_idx],
+                            f"Unexpected ground truth object at {i}",
+                        )
+                    else:
+                        # In this case, there is no threshold
+                        self.assertIsNone(
+                            object_result_.ground_truth_object,
+                            "ground truth must be None",
+                        )
