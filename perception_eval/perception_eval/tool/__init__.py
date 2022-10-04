ac134398dd95cc1dcc58f12ff24b999ea4f50671
ktro2828 <kotaro.uetake@tier4.jp>
2022-10-04 11:31:32 +0900

    feat: add EM algorithm for GMM
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- perception_eval/perception_eval/tool/__init__.py
+++ perception_eval/perception_eval/tool/__init__.py
@@ -1,5 +1,6 @@
+from .gmm import Gmm
 from .perception_performance_analyzer import MatchingStatus
 from .perception_performance_analyzer import PerceptionPerformanceAnalyzer
 from .perception_performance_analyzer import PlotMode
 
-__all__ = ["PerceptionPerformanceAnalyzer", "PlotMode", "MatchingStatus"]
+__all__ = ["PerceptionPerformanceAnalyzer", "PlotMode", "MatchingStatus", "Gmm"]
