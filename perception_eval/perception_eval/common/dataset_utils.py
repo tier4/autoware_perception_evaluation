541756f8486096696e45a084ae947f350201d1e6
Kotaro Uetake <60615504+ktro2828@users.noreply.github.com>
2023-10-06 10:20:12 +0900

    ci(pre-commit): update pre-commit ci (#97)
    * ci: update pre-commit ci
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * style(pre-commit): autofix
    
    ---------
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    Co-authored-by: ktro2828 <ktro2828@users.noreply.github.com>

--- perception_eval/perception_eval/common/dataset_utils.py
+++ perception_eval/perception_eval/common/dataset_utils.py
@@ -27,9 +27,6 @@ import numpy as np
 from nuscenes.nuscenes import NuScenes
 from nuscenes.prediction.helper import PredictHelper
 from nuscenes.utils.data_classes import Box
-from PIL import Image
-from pyquaternion.quaternion import Quaternion
-
 from perception_eval.common.evaluation_task import EvaluationTask
 from perception_eval.common.label import Label
 from perception_eval.common.label import LabelConverter
@@ -41,6 +38,8 @@ from perception_eval.common.schema import FrameID
 from perception_eval.common.schema import Visibility
 from perception_eval.common.shape import Shape
 from perception_eval.common.shape import ShapeType
+from PIL import Image
+from pyquaternion.quaternion import Quaternion
 
 from . import dataset
 
