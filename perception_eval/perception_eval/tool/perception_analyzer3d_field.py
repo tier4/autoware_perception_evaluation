f382ad94d1f4072b888f2984fd49c5b4cffa2d5f
Taekjin LEE <taekjin.lee@tier4.jp>
2023-11-15 14:38:41 +0900

    feat: custom grid definition, mesh plot

--- perception_eval/perception_eval/tool/perception_analyzer3d_field.py
+++ perception_eval/perception_eval/tool/perception_analyzer3d_field.py
@@ -32,74 +32,73 @@ from .utils import get_metrics_info
 
 
 # Perception Field
-class GridConfig:
-    def __init__(self) -> None:
-        self.max_x = 100.0
-        self.min_x = -100.0
-        self.interval_x = 10.0
-        self.offset_x = 5.0
-        self.num_division_x = int((self.max_x - self.min_x) / self.interval_x)
-        self.max_y = 100.0
-        self.min_y = -100.0
-        self.interval_y = 10.0
-        self.offset_y = 5.0
-        self.num_division_y = int((self.max_y - self.min_y) / self.interval_y)
-
 class PerceptionFieldXY:
-    def __init__(self, evaluation_config: PerceptionEvaluationConfig) -> None:
-        self.__config = evaluation_config
-
-        # set grid
-        self.__config.grid = GridConfig()
+    def __init__(self, evaluation_config: PerceptionEvaluationConfig, 
+                 grid_axis_x : np.ndarray, 
+                 grid_axis_y : np.ndarray
+                 ) -> None:
         
-        self.grid_x = np.linspace(
-            self.__config.grid.min_x + self.__config.grid.offset_x,
-            self.__config.grid.max_x + self.__config.grid.offset_x,
-            self.__config.grid.num_division_x + 1,
-        )
-        self.grid_y = np.linspace(
-            self.__config.grid.min_y + self.__config.grid.offset_y,
-            self.__config.grid.max_y + self.__config.grid.offset_y,
-            self.__config.grid.num_division_y + 1,
-        )
+        self.__config : PerceptionEvaluationConfig = evaluation_config
+        # Set statistics parameters
+        self.config_statistic_min_numb : int = 4 # minimum number of pairs to calculate statistics
 
-        # arrays represent surface, including outside of grid points
-        # therefore, the size of array is (number of grid points + 1)
-        self.nx = self.grid_x.size + 1 
-        self.ny = self.grid_y.size + 1 
-        # print("grid size", self.nx, self.ny)
+        # Set grid mesh
+        #   Grid axis 1 dimenstion and is defined as follows:
+        #      0     1     2          grid points, positions 
+        #  ----|-----|-----|---->
+        #   0     1     2     3       cell array index, 0 and 3 are outside of grid points
+        #      |  |     |  |          cell center positions are defined as average of grid points                           
+        #      |           |          cell positions of 0 and 3 are the boundary of the grid 
 
-        # set statistics parameters
-        self.config_statistic_min_numb = 4 # minimum number of pairs to calculate statistics
+        self.grid_axis_x : np.ndarray = np.sort(grid_axis_x)
+        self.grid_axis_y : np.ndarray = np.sort(grid_axis_y)
 
-        # define layers
-        self.x = np.zeros((self.nx, self.ny))
-        self.y = np.zeros((self.nx, self.ny))
-        self.yaw = np.zeros((self.nx, self.ny))
-        self.vx = np.zeros((self.nx, self.ny))
-        self.vy = np.zeros((self.nx, self.ny))
+        # Generate mesh cells
+        #   The mesh is created by two 1D arrays of grid points
+        self.cell_pos_x : np.ndarray = np.append(self.grid_axis_x, self.grid_axis_x[-1])
+        self.cell_pos_y : np.ndarray = np.append(self.grid_axis_y, self.grid_axis_y[-1])
+        self.cell_pos_x[1:-2] = (self.cell_pos_x[0:-3] +  self.cell_pos_x[1:-2]) / 2
+        self.cell_pos_y[1:-2] = (self.cell_pos_y[0:-3] +  self.cell_pos_y[1:-2]) / 2
 
-        self.num = np.zeros((self.nx, self.ny))
-        self.ratio_valid = np.ones((self.nx, self.ny))
-        self.num_tp = np.zeros((self.nx, self.ny))
-        self.num_tn = np.zeros((self.nx, self.ny))
-        self.num_fp = np.zeros((self.nx, self.ny))
-        self.num_fn = np.zeros((self.nx, self.ny))
-        self.ratio_tp = np.zeros((self.nx, self.ny))
-        self.ratio_tn = np.zeros((self.nx, self.ny))
-        self.ratio_fp = np.zeros((self.nx, self.ny))
-        self.ratio_fn = np.zeros((self.nx, self.ny))
+        # cell positions are defined as average of grid points
+        self.mesh_center_x, self.mesh_center_y = np.meshgrid(self.cell_pos_x, self.cell_pos_y, indexing='ij')
 
-        self.num_pair = np.zeros((self.nx, self.ny))
-        self.pair_valid = np.ones((self.nx, self.ny))
-        self.error_x_mean = np.zeros((self.nx, self.ny))
-        self.error_x_std = np.zeros((self.nx, self.ny))
-        self.error_y_mean = np.zeros((self.nx, self.ny))
-        self.error_y_std = np.zeros((self.nx, self.ny))
-        self.error_delta_mean = np.zeros((self.nx, self.ny))
-        self.error_delta_std = np.zeros((self.nx, self.ny))
-        self.error_yaw_mean = np.zeros((self.nx, self.ny))
-        self.error_yaw_std = np.zeros((self.nx, self.ny))
+        # Set layer array size
+        #   arrays represent surface, including outside of grid points
+        #   therefore, the size of array is (number of grid points + 1)
+        self.nx : int = self.mesh_center_x.shape[0]
+        self.ny : int = self.mesh_center_x.shape[1]
+
+        # Define layers
+        self.x : np.ndarray = np.zeros((self.nx, self.ny))
+        self.y : np.ndarray = np.zeros((self.nx, self.ny))
+        self.yaw : np.ndarray = np.zeros((self.nx, self.ny))
+        self.vx : np.ndarray = np.zeros((self.nx, self.ny))
+        self.vy : np.ndarray = np.zeros((self.nx, self.ny))
+
+        self.num : np.ndarray = np.zeros((self.nx, self.ny))
+        self.ratio_valid : np.ndarray = np.ones((self.nx, self.ny))
+        self.num_tp : np.ndarray = np.zeros((self.nx, self.ny))
+        self.num_tn : np.ndarray = np.zeros((self.nx, self.ny))
+        self.num_fp : np.ndarray = np.zeros((self.nx, self.ny))
+        self.num_fn : np.ndarray = np.zeros((self.nx, self.ny))
+        self.ratio_tp : np.ndarray = np.zeros((self.nx, self.ny))
+        self.ratio_tn : np.ndarray = np.zeros((self.nx, self.ny))
+        self.ratio_fp : np.ndarray = np.zeros((self.nx, self.ny))
+        self.ratio_fn : np.ndarray = np.zeros((self.nx, self.ny))
+
+        self.num_pair : np.ndarray = np.zeros((self.nx, self.ny))
+        self.pair_valid : np.ndarray = np.ones((self.nx, self.ny))
+        self.error_x_mean : np.ndarray = np.zeros((self.nx, self.ny))
+        self.error_x_std : np.ndarray = np.zeros((self.nx, self.ny))
+        self.error_y_mean : np.ndarray = np.zeros((self.nx, self.ny))
+        self.error_y_std : np.ndarray = np.zeros((self.nx, self.ny))
+        self.error_delta_mean : np.ndarray = np.zeros((self.nx, self.ny))
+        self.error_delta_std : np.ndarray = np.zeros((self.nx, self.ny))
+        self.error_yaw_mean : np.ndarray = np.zeros((self.nx, self.ny))
+        self.error_yaw_std : np.ndarray = np.zeros((self.nx, self.ny))
+
+
 
     def processMeans(self)->None:
         self.ratio_valid = self.num > 0
@@ -108,8 +107,8 @@ class PerceptionFieldXY:
         self.yaw[self.ratio_valid] = np.divide(self.yaw[self.ratio_valid], self.num[self.ratio_valid])
         self.vx[self.ratio_valid] = np.divide(self.vx[self.ratio_valid], self.num[self.ratio_valid])
         self.vy[self.ratio_valid] = np.divide(self.vy[self.ratio_valid], self.num[self.ratio_valid])
-        self.x[~self.ratio_valid] = np.nan
-        self.y[~self.ratio_valid] = np.nan
+        self.x[~self.ratio_valid] = self.mesh_center_x[~self.ratio_valid]
+        self.y[~self.ratio_valid] = self.mesh_center_y[~self.ratio_valid]
         self.yaw[~self.ratio_valid] = np.nan
         self.vx[~self.ratio_valid] = np.nan
         self.vy[~self.ratio_valid] = np.nan
@@ -144,37 +143,55 @@ class PerceptionFieldXY:
         self.error_delta_mean[~self.pair_valid] = np.nan
         self.error_delta_std[~self.pair_valid] = np.nan
 
+    # Get index
+    def getGridIndex(self, pos_x: float, pos_y: float) -> Tuple[int, int]:
+        for idx, x in enumerate(self.grid_axis_x):
+            if pos_x > x:
+                idx_x = idx + 1
+        if pos_x < self.grid_axis_x[0]:
+            idx_x = 0
+        if idx_x == self.nx:
+            idx_x = self.nx - 1
 
+        for idx, y in enumerate(self.grid_axis_y):
+            if pos_y > y:
+                idx_y = idx +1
+        if pos_y < self.grid_axis_y[0]:
+            idx_y = 0
+        if idx_y == self.ny:
+            idx_y = self.ny - 1
+        
+        return idx_x, idx_y
+   
 # Analyzer
 class PerceptionAnalyzer3DField(PerceptionAnalyzer3D):
-    """An analyzer class for 3D perception field evaluation results.
+    """An analyzer class for 3D perception field evaluation results."""
 
-    Attributes:
-
-    Args:
-
-    """
-
-    def analyze(self, **kwargs) -> Tuple[Optional[PerceptionFieldXY], Optional[PerceptionFieldXY]]:
-        """Analyze TP/FP/FN ratio, metrics score, error. If there is no DataFrame to be able to analyze returns None.
+    def analyzeXY(self, 
+                  label_axis_x:str, grid_axis_x:np.ndarray,
+                  label_axis_y:str, grid_axis_y:np.ndarray,
+                  **kwargs) -> Tuple[Optional[PerceptionFieldXY], Optional[PerceptionFieldXY]]:
+        """Analyze 3D perception field evaluation results.
 
         Args:
-            **kwargs: Specify scene, frame, area or uuid.
-
+            label_axis_x (str): label of x axis.
+            grid_axis_x (np.ndarray): grid axis of x axis.
+            label_axis_y (str): label of y axis.
+            grid_axis_y (np.ndarray): grid axis of y axis.
+            **kwargs: Specify scene, frame, area or uuid.  
+        
         Returns:
             error_field (Optional[PerceptionFieldXY]): Field of errors.
             uncertainty_field (Optional[PerceptionFieldXY]): Field of uncertainties.
+
         """
+
+        # Extrack data
         df: pd.DataFrame = self.get(**kwargs)
 
-        # initialize
-        # detect_field : PerceptionFieldXY = PerceptionFieldXY(self.config)
-        error_field : PerceptionFieldXY = PerceptionFieldXY(self.config)
-        uncertainty_field : PerceptionFieldXY = PerceptionFieldXY(self.config)
-
-        # set condition
-        grid_idx_x : str = "x"
-        grid_idx_y : str = "y"
+        # Initialize fields
+        error_field : PerceptionFieldXY = PerceptionFieldXY(self.config, grid_axis_x, grid_axis_y)
+        uncertainty_field : PerceptionFieldXY = PerceptionFieldXY(self.config, grid_axis_x, grid_axis_y)
 
         # loop for each frame
         for _, item in df.groupby(level=0):
@@ -185,18 +202,15 @@ class PerceptionAnalyzer3DField(PerceptionAnalyzer3D):
             
             if "ground_truth" in pair.index:
                 gt = pair.loc["ground_truth"]
-                is_gt_valid = ~np.isnan(gt[grid_idx_x]) and ~np.isnan(gt[grid_idx_y])
+                is_gt_valid = ~np.isnan(gt[label_axis_x]) and ~np.isnan(gt[label_axis_y])
                 is_paired = is_gt_valid & (gt["status"] == MatchingStatus.TP.value)
             if "estimation" in pair.index:
                 est = pair.loc["estimation"]
-                is_est_valid = ~np.isnan(est[grid_idx_x]) and ~np.isnan(est[grid_idx_y])
+                is_est_valid = ~np.isnan(est[label_axis_x]) and ~np.isnan(est[label_axis_y])
                 is_paired = is_paired & is_est_valid
 
-            # if est["status"]!=MatchingStatus.FN:
-            #     print("status", gt["status"], est["status"])
-
             if is_gt_valid:
-                idx_gt_x, idx_gt_y = self.getGridIndex(gt[grid_idx_x], gt[grid_idx_y])
+                idx_gt_x, idx_gt_y = error_field.getGridIndex(gt[label_axis_x], gt[label_axis_y])
                 error_field.num[idx_gt_x, idx_gt_y] += 1
 
                 error_field.x[idx_gt_x, idx_gt_y] += gt["x"]
@@ -217,7 +231,7 @@ class PerceptionAnalyzer3DField(PerceptionAnalyzer3D):
                     pass
 
             if is_est_valid:
-                idx_est_x, idx_est_y = self.getGridIndex(est[grid_idx_x], est[grid_idx_y])
+                idx_est_x, idx_est_y = uncertainty_field.getGridIndex(est[label_axis_x], est[label_axis_y])
                 uncertainty_field.num[idx_gt_x, idx_gt_y] += 1
 
                 uncertainty_field.x[idx_est_x, idx_est_y] += est["x"]
@@ -248,7 +262,7 @@ class PerceptionAnalyzer3DField(PerceptionAnalyzer3D):
                 elif error_yaw < -np.pi:
                     error_yaw += 2*np.pi
 
-                idx_gt_x, idx_gt_y = self.getGridIndex(gt[grid_idx_x], gt[grid_idx_y])
+                idx_gt_x, idx_gt_y = error_field.getGridIndex(gt[label_axis_x], gt[label_axis_y])
                 error_field.num_pair[idx_gt_x, idx_gt_y] += 1
                 error_field.error_x_mean[idx_gt_x, idx_gt_y] += error_x
                 error_field.error_x_std[idx_gt_x, idx_gt_y] += error_x ** 2
@@ -259,7 +273,7 @@ class PerceptionAnalyzer3DField(PerceptionAnalyzer3D):
                 error_field.error_delta_mean[idx_gt_x, idx_gt_y] += error_delta
                 error_field.error_delta_std[idx_gt_x, idx_gt_y] += error_delta ** 2
                 
-                idx_est_x, idx_est_y = self.getGridIndex(est[grid_idx_x], est[grid_idx_y])
+                idx_est_x, idx_est_y = uncertainty_field.getGridIndex(est[label_axis_x], est[label_axis_y])
                 uncertainty_field.num_pair[idx_est_x, idx_est_y] += 1
                 uncertainty_field.error_x_mean[idx_est_x, idx_est_y] += -error_x
                 uncertainty_field.error_x_std[idx_est_x, idx_est_y] += error_x ** 2
@@ -285,12 +299,28 @@ class PerceptionAnalyzer3DField(PerceptionAnalyzer3D):
         return error_field, uncertainty_field
 
 
-    # get index
-    def getGridIndex(self, pos_x: float, pos_y: float) -> Tuple[int, int]:
-        idx_x = int((pos_x - self.config.grid.min_x) / self.config.grid.interval_x)
-        idx_y = int((pos_y - self.config.grid.min_y) / self.config.grid.interval_y)
-        return idx_x, idx_y
-   
+    def analyze(self, **kwargs) -> Tuple[Optional[PerceptionFieldXY], Optional[PerceptionFieldXY]]:
+        """Analyze TP/FP/FN ratio, metrics score, error. If there is no DataFrame to be able to analyze returns None.
+
+        Args:
+            **kwargs: Specify scene, frame, area or uuid.
+
+        Returns:
+            error_field (Optional[PerceptionFieldXY]): Field of errors.
+            uncertainty_field (Optional[PerceptionFieldXY]): Field of uncertainties.
+        """
+
+        # Initialize fields
+        grid_axis_x : np.ndarray = np.array([-90, -60, -40, -30, -20, -10, -5, 5, 10, 20, 30, 40, 60, 90])
+        grid_axis_y : np.ndarray = np.array([-90, -60, -40, -30, -20, -10, -5, 5, 10, 20, 30, 40, 60, 90])
+
+        # Set condition
+        label_axis_x : str = "x"
+        label_axis_y : str = "y"
+
+        return self.analyzeXY(label_axis_x, label_axis_y, grid_axis_x, grid_axis_y, **kwargs)
+
+
 
 # Visualizer
 
--- perception_eval/perception_eval/tool/perception_field_analyze.py
+++ perception_eval/perception_eval/tool/perception_field_analyze.py
@@ -22,41 +22,62 @@ from perception_eval.tool import PerceptionAnalyzer3DField
 
 import matplotlib.pyplot as plt
 import simplejson as json
+import numpy as np
 
 class PerceptionFieldPlot:
     def __init__(self, figname:str)-> None:
         self.name : str = figname
-        self.figure = plt.figure(self.name)
+        self.figure = plt.figure(self.name, figsize=(10, 8))
         self.ax = self.figure.add_subplot(111)
+        
         self.ax.set_aspect('equal')
     
+    def pcolormesh(self, x, y, z, **kwargs):
+        self.cs = self.ax.pcolormesh(x, y, z, 
+                                     shading='nearest',
+                                     **kwargs)
+        self.cbar = self.figure.colorbar(self.cs)
+        self.setXY()
+
+    def contourf(self, x, y, z, **kwargs):
+        self.cs = self.ax.contourf(x, y, z, **kwargs)
+        self.ax.contour(self.cs, colors='k')
+        self.cbar = self.figure.colorbar(self.cs)
+        self.setXY()
+
+    def setXY(self)->None:
+        self.ax.set_xlabel("x [m]")
+        self.ax.set_ylabel("y [m]")
+        self.ax.set_aspect('equal')
+        self.ax.set_xlim([-110, 110])
+        self.ax.set_ylim([-110, 110])
+        self.ax.grid(c='k', ls='-', alpha=0.3)
 
 def visualize(field:PerceptionAnalyzer3DField, result_root_directory:str)->None:
     
     # Plot
     figures = []
     figures.append(PerceptionFieldPlot("num_pair"))
-    figures[-1].ax.matshow(field.num_pair, cmap='jet')
-  
-    figures.append(PerceptionFieldPlot("delta_mean"))
-    figures[-1].ax.matshow(field.error_delta_mean, cmap='jet')
+    figures[-1].pcolormesh(field.mesh_center_x, field.mesh_center_y, field.num_pair, vmin=0)
 
     figures.append(PerceptionFieldPlot("ratio_tp"))
-    figures[-1].ax.matshow(field.ratio_tp, cmap='jet')
+    figures[-1].pcolormesh(field.mesh_center_x, field.mesh_center_y, field.ratio_tp, vmin=0, vmax=1)
 
     figures.append(PerceptionFieldPlot("ratio_fp"))
-    figures[-1].ax.matshow(field.ratio_fp, cmap='jet')
+    figures[-1].pcolormesh(field.mesh_center_x, field.mesh_center_y, field.ratio_fp, vmin=0, vmax=1)
 
-    figures.append(PerceptionFieldPlot("delta_mean_map"))
-    cs = figures[-1].ax.contourf(field.x, field.y, field.error_delta_mean, cmap='jet', corner_mask=False)
-    figures[-1].ax.contour(cs, colors='k')
-    figures[-1].ax.grid(c='k', ls='-', alpha=0.3)
-    cbar = figures[-1].figure.colorbar(cs)
-    figures[-1].ax.set_xlabel("x [m]")
-    figures[-1].ax.set_ylabel("y [m]")
-    figures[-1].ax.set_aspect('equal')
-    figures[-1].ax.set_xlim([-110, 110])
-    figures[-1].ax.set_ylim([-110, 110])
+    # figures.append(PerceptionFieldPlot("delta_mean"))
+    # figures[-1].ax.matshow(field.error_delta_mean, cmap='jet')
+
+    # figures.append(PerceptionFieldPlot("delta_mean_contour"))
+    # levels = [0,0.1,0.2,0.3,0.5,1,2,5,10]
+    # figures[-1].contourf(field.x, field.y, field.error_delta_mean, 
+    #                      levels=levels, cmap='jet', corner_mask=False)
+
+    figures.append(PerceptionFieldPlot("delta_mean_mesh"))
+    figures[-1].pcolormesh(field.x, field.y, field.error_delta_mean, 
+                           vmin=0, vmax = np.nanmax(field.error_delta_mean))
+    cs = figures[-1].ax.scatter(field.x, field.y, marker='+',c='r', s=10)
 
     # Save result
     plot_file_path = Path(result_root_directory, "plot")
@@ -82,16 +103,31 @@ class PerceptionLoadDatabaseResult:
         # Analyze
         # conditions: object class, grid type, error/uncertainty...
 
-        # All classes
-        error_field, uncertainty_field = analyzer.analyze()
+        # Defalult analysis : all object class, all grid type, error
+        # error_field, uncertainty_field = analyzer.analyze()
 
         # Specific class
         # labels: car, truck, bicycle, pedestrian, motorbike
         # src/simulator/perception_eval/docs/en/perception/label.md
         # error_field, _ = analyzer.analyze(label="car") 
 
-        print(analyzer.df)
-        # print(analyzer.df.columns)
+
+        # Specific grid type - 2D xy grid
+        # Initialize fields
+        grid_axis_x : np.ndarray = np.array([-90, -60, -40, -30, -20, -10, -5, 5, 10, 20, 30, 40, 60, 90])
+        grid_axis_y : np.ndarray = np.array([-90, -60, -40, -30, -20, -10, -5, 5, 10, 20, 30, 40, 60, 90])
+
+        # Set condition
+        error_field, uncertainty_field = analyzer.analyzeXY("x",grid_axis_x,"y",grid_axis_y)
+
+        # Specific grid type - 1D range grid
+
+
+
+
+
+        # print(analyzer.df)
+        print(analyzer.df.columns)
 
         # Plot
         visualize(error_field, result_root_directory)
