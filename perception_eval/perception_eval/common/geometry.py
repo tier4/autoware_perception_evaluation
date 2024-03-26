fix: fix to transform object from map coords to base link coords in plane distance calculation
Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

perception_eval/perception_eval/common/geometry.py
@@ -19,11 +19,8 @@ from typing import Union
 
 import numpy as np
 from perception_eval.common.object2d import DynamicObject2D
-from perception_eval.common.object2d import Roi
 from perception_eval.common.object import DynamicObject
 from perception_eval.common.object import ObjectState
-from perception_eval.common.point import distance_points
-from perception_eval.common.point import distance_points_bev
 from pyquaternion import Quaternion
 
 # Type aliases
@@ -203,3 +200,19 @@ def interpolate_dynamic_object2d(
     else:
         output_object = deepcopy(object_1)
     return output_object
+
+
+def transform_map2ego(position: Tuple[float, float, float], ego2map: np.ndarray) -> Tuple[float, float, float]:
+    """Transform map coordinates to base link coordinates.
+
+    Args:
+        position (Tuple[float, float, float]): Source xyz position w.r.t map coordinates.
+        ego2map (np.ndarray): Matrix to transform base link to map coordinates.
+
+    Returns:
+        Tuple[float, float, float]: Transformed xyz position.
+    """
+    assert len(position) == 3
+    src = np.append(position, 1.0)
+    dst = np.linalg.inv(ego2map).dot(src)[:3]
+    return tuple(dst.tolist())

perception_eval/perception_eval/evaluation/matching/object_matching.py
@@ -23,9 +23,11 @@ import numpy as np
 from perception_eval.common import distance_objects
 from perception_eval.common import distance_points_bev
 from perception_eval.common import ObjectType
+from perception_eval.common.geometry import transform_map2ego
 from perception_eval.common.object import DynamicObject
 from perception_eval.common.point import get_point_left_right
 from perception_eval.common.point import polygon_to_list
+from perception_eval.common.schema import FrameID
 from shapely.geometry import Polygon
 
 
@@ -66,16 +68,14 @@ class MatchingMethod(ABC):
     mode: MatchingMode
 
     @abstractmethod
-    def __init__(
-        self,
-        estimated_object: ObjectType,
-        ground_truth_object: Optional[ObjectType],
-    ) -> None:
+    def __init__(self, estimated_object: ObjectType, ground_truth_object: Optional[ObjectType], **kwargs) -> None:
         if ground_truth_object is not None:
             assert type(estimated_object) == type(ground_truth_object)
+
         self.value: Optional[float] = self._calculate_matching_score(
             estimated_object=estimated_object,
             ground_truth_object=ground_truth_object,
+            **kwargs,
         )
 
     @abstractmethod
@@ -83,6 +83,7 @@ class MatchingMethod(ABC):
         self,
         estimated_object: ObjectType,
         ground_truth_object: Optional[ObjectType],
+        **kwargs,
     ) -> Optional[float]:
         pass
 
@@ -121,11 +122,7 @@ class CenterDistanceMatching(MatchingMethod):
 
     mode: MatchingMode = MatchingMode.CENTERDISTANCE
 
-    def __init__(
-        self,
-        estimated_object: ObjectType,
-        ground_truth_object: Optional[ObjectType],
-    ) -> None:
+    def __init__(self, estimated_object: ObjectType, ground_truth_object: Optional[ObjectType], **kwargs) -> None:
         super().__init__(estimated_object=estimated_object, ground_truth_object=ground_truth_object)
 
     def is_better_than(
@@ -194,10 +191,13 @@ class PlaneDistanceMatching(MatchingMethod):
         self,
         estimated_object: DynamicObject,
         ground_truth_object: Optional[DynamicObject],
+        ego2map: Optional[np.ndarray] = None,
     ) -> None:
         self.ground_truth_nn_plane: Optional[Tuple[Tuple[float, float, float], Tuple[float, float, float]]] = None
         self.estimated_nn_plane: Optional[Tuple[Tuple[float, float, float], Tuple[float, float, float]]] = None
-        super().__init__(estimated_object=estimated_object, ground_truth_object=ground_truth_object)
+        if estimated_object.frame_id == FrameID.MAP:
+            assert ego2map is not None, "ego2map must be specified for map frame objects in PlaneDistanceMatching."
+        super().__init__(estimated_object=estimated_object, ground_truth_object=ground_truth_object, ego2map=ego2map)
 
     def is_better_than(
         self,
@@ -222,6 +222,7 @@ class PlaneDistanceMatching(MatchingMethod):
         self,
         estimated_object: DynamicObject,
         ground_truth_object: Optional[DynamicObject],
+        ego2map: Optional[np.ndarray],
     ) -> Optional[float]:
         """Calculate plane distance between estimation and ground truth and NN planes.
 
@@ -247,8 +248,12 @@ class PlaneDistanceMatching(MatchingMethod):
         gt_corners = np.array(polygon_to_list(gt_footprint, include_first=True))
 
         # Calculate min distance from ego vehicle
-        # TODO: for objects with respect to map coords need to transform to base link coords
-        gt_distances: np.ndarray = np.linalg.norm(gt_corners[:-1, :2], axis=1)
+        if ground_truth_object.frame_id == FrameID.MAP:
+            tmp_corners = np.array([transform_map2ego(c, ego2map) for c in gt_corners])
+            gt_min_points = 0.5 * (tmp_corners[:-1, :2] + tmp_corners[1:, :2])
+        else:
+            gt_min_points = 0.5 * (gt_corners[:-1, :2] + gt_corners[1:, :2])
+        gt_distances: np.ndarray = np.linalg.norm(gt_min_points, axis=1)
 
         min_plane_distance = float("inf")
         min_indices = np.where(gt_distances == gt_distances.min())[0]
@@ -290,11 +295,7 @@ class IOU2dMatching(MatchingMethod):
 
     mode: MatchingMode = MatchingMode.IOU2D
 
-    def __init__(
-        self,
-        estimated_object: ObjectType,
-        ground_truth_object: Optional[ObjectType],
-    ) -> None:
+    def __init__(self, estimated_object: ObjectType, ground_truth_object: Optional[ObjectType], **kwargs) -> None:
         super().__init__(estimated_object=estimated_object, ground_truth_object=ground_truth_object)
 
     def is_better_than(
@@ -377,11 +378,7 @@ class IOU3dMatching(MatchingMethod):
 
     mode: MatchingMode = MatchingMode.IOU3D
 
-    def __init__(
-        self,
-        estimated_object: DynamicObject,
-        ground_truth_object: Optional[DynamicObject],
-    ) -> None:
+    def __init__(self, estimated_object: DynamicObject, ground_truth_object: Optional[DynamicObject], **kwargs) -> None:
         super().__init__(estimated_object=estimated_object, ground_truth_object=ground_truth_object)
 
     def is_better_than(

perception_eval/perception_eval/evaluation/result/object_result.py
@@ -55,6 +55,7 @@ class DynamicObjectWithPerceptionResult:
         self,
         estimated_object: ObjectType,
         ground_truth_object: Optional[ObjectType],
+        ego2map: Optional[np.ndarray] = None,
     ) -> None:
         """[summary]
         Evaluation result for an object estimated object.
@@ -62,6 +63,7 @@ class DynamicObjectWithPerceptionResult:
         Args:
             estimated_object (ObjectType): The estimated object by inference like CenterPoint
             ground_truth_objects (Optional[ObjectType]): The list of Ground truth objects
+            ego2map (Optional[np.ndarray]): Matrix to transform from base link coords to map coords. Defaults to None.
         """
         if ground_truth_object is not None:
             assert type(estimated_object) == type(
@@ -92,6 +94,7 @@ class DynamicObjectWithPerceptionResult:
             self.plane_distance: PlaneDistanceMatching = PlaneDistanceMatching(
                 self.estimated_object,
                 self.ground_truth_object,
+                ego2map=ego2map,
             )
         else:
             self.iou_3d = None
@@ -272,6 +275,7 @@ def get_object_results(
     allow_matching_unknown: bool = True,
     matching_mode: MatchingMode = MatchingMode.CENTERDISTANCE,
     matchable_thresholds: Optional[List[float]] = None,
+    ego2map: Optional[np.ndarray] = None,
 ) -> List[DynamicObjectWithPerceptionResult]:
     """Returns list of DynamicObjectWithPerceptionResult.
 
@@ -287,6 +291,7 @@ def get_object_results(
         ground_truth_objects (List[ObjectType]): Ground truth objects list.
         matching_mode (MatchingMode): MatchingMode instance.
         matchable_thresholds (Optional[List[float]]): Thresholds to be
+        ego2map (Optional[np.ndarray]): Matrix to transform from base link coords to map coords. Defaults to None.
 
     Returns:
         object_results (List[DynamicObjectWithPerceptionResult]): Object results list.
@@ -297,7 +302,7 @@ def get_object_results(
 
     # There is no GT and not FP validation (= all FP)
     if not ground_truth_objects and evaluation_task.is_fp_validation() is False:
-        return _get_fp_object_results(estimated_objects)
+        return _get_fp_object_results(estimated_objects, ego2map)
 
     assert isinstance(
         ground_truth_objects[0], type(estimated_objects[0])
@@ -314,6 +319,7 @@ def get_object_results(
         matching_method_module,
         target_labels,
         matchable_thresholds,
+        ego2map,
     )
 
     # assign correspond GT to estimated objects
@@ -339,6 +345,7 @@ def get_object_results(
         object_result_ = DynamicObjectWithPerceptionResult(
             estimated_object=est_obj_,
             ground_truth_object=gt_obj_,
+            ego2map=ego2map,
         )
         object_results.append(object_result_)
 
@@ -346,7 +353,7 @@ def get_object_results(
     # when there are rest of estimated objects, they all are FP.
     # Otherwise, they all are ignored
     if len(estimated_objects_) > 0 and evaluation_task.is_fp_validation() is False:
-        object_results += _get_fp_object_results(estimated_objects_)
+        object_results += _get_fp_object_results(estimated_objects_, ego2map)
 
     return object_results
 
@@ -384,6 +391,7 @@ def _get_object_results_with_id(
                     DynamicObjectWithPerceptionResult(
                         estimated_object=est_object,
                         ground_truth_object=gt_object,
+                        ego2map=None,
                     )
                 )
                 estimated_objects_.remove(est_object)
@@ -391,18 +399,20 @@ def _get_object_results_with_id(
 
     # when there are rest of estimated objects, they all are FP.
     if len(estimated_objects_) > 0:
-        object_results += _get_fp_object_results(estimated_objects_)
+        object_results += _get_fp_object_results(estimated_objects_, None)
 
     return object_results
 
 
 def _get_fp_object_results(
     estimated_objects: List[ObjectType],
+    ego2map: Optional[np.ndarray] = None,
 ) -> List[DynamicObjectWithPerceptionResult]:
     """Returns the list of DynamicObjectWithPerceptionResult that have no ground truth.
 
     Args:
         estimated_objects (List[ObjectType]): Estimated objects list.
+        ego2map (Optional[np.ndarray]): Matrix to transform from base link coords to map coords. Defaults to None.
 
     Returns:
         object_results (List[DynamicObjectWithPerceptionResult]): FP object results list.
@@ -412,6 +422,7 @@ def _get_fp_object_results(
         object_result_: DynamicObjectWithPerceptionResult = DynamicObjectWithPerceptionResult(
             estimated_object=est_obj_,
             ground_truth_object=None,
+            ego2map=ego2map,
         )
         object_results.append(object_result_)
 
@@ -453,6 +464,7 @@ def _get_score_table(
     matching_method_module: Callable,
     target_labels: Optional[List[LabelType]],
     matchable_thresholds: Optional[List[float]],
+    ego2map: Optional[np.ndarray],
 ) -> np.ndarray:
     """Returns score table, in shape (num_estimation, num_ground_truth).
 
@@ -462,7 +474,8 @@ def _get_score_table(
         allow_matching_unknown (bool): Indicates whether allow to match with unknown label.
         matching_method_module (Callable): MatchingMethod instance.
         target_labels (Optional[List[LabelType]]): Target labels to be evaluated.
-        matching_thresholds (Optional[List[float]]): List of thresholds
+        matching_thresholds (Optional[List[float]]): List of thresholds.
+        ego2map (Optional[np.ndarray]): Matrix to transform from base link coords to map coords.
 
     Returns:
         score_table (numpy.ndarray): in shape (num_estimation, num_ground_truth).
@@ -489,7 +502,7 @@ def _get_score_table(
                 )
 
                 matching_method: MatchingMethod = matching_method_module(
-                    estimated_object=est_obj, ground_truth_object=gt_obj
+                    estimated_object=est_obj, ground_truth_object=gt_obj, ego2map=ego2map
                 )
 
                 if threshold is None or (threshold is not None and matching_method.is_better_than(threshold)):