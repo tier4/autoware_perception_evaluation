bffcdd83fb71eb136d32eb6a215626de2a867674
MasatoSaeki <masato.saeki@tier4.jp>
2024-06-28 20:25:44 +0900

    update test
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>

--- perception_eval/test/perception_lsim.py
+++ perception_eval/test/perception_lsim.py
@@ -128,7 +128,6 @@ class PerceptionLSimMoc:
             unix_time=unix_time,
             ground_truth_now_frame=ground_truth_now_frame,
             estimated_objects=estimated_objects,
-            ros_critical_ground_truth_objects=ros_critical_ground_truth_objects,
             critical_object_filter_config=critical_object_filter_config,
             frame_pass_fail_config=frame_pass_fail_config,
         )
