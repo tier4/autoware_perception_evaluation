c3255c8948503d724f5ae9af5f30503f449a65cb
Masato Saeki <78376491+MasatoSaeki@users.noreply.github.com>
2024-10-02 00:55:30 -0400

    feat(perception): add option on TLR in order to perform whether uuid check first or not. (#189)
    * add patch
    
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>
    
    * style(pre-commit): autofix
    
    * change variable name
    
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>
    
    ---------
    
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>
    Co-authored-by: MasatoSaeki <MasatoSaeki@users.noreply.github.com>

--- perception_eval/perception_eval/config/perception_evaluation_config.py
+++ perception_eval/perception_eval/config/perception_evaluation_config.py
@@ -190,6 +190,7 @@ class PerceptionEvaluationConfig(_EvaluationConfigBase):
             "min_point_numbers": min_point_numbers,
             "confidence_threshold_list": confidence_threshold_list,
             "target_uuids": target_uuids,
+            "uuid_matching_first": e_cfg.get("uuid_matching_first", False),
         }
 
         m_params: Dict[str, Any] = {
