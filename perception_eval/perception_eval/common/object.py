fix: path error computation
Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

perception_eval/perception_eval/common/object.py
@@ -365,17 +365,13 @@ class DynamicObject:
         if other is None:
             return None
 
-        path_errors: List[List[List[float]]] = []
-        for self_path, other_path in zip(self.predicted_paths, other.predicted_paths):
-            if self_path is None or other_path is None:
-                continue
-            min_length = min(len(self_path), len(other_path))
-            self_path, other_path = self_path[:min_length], other_path[:min_length]
-            err: List[Tuple[float, float, float]] = [
-                self_state.get_position_error(other_state) for self_state, other_state in zip(self_path, other_path)
-            ]  # (T, 3)
-            path_errors.append(err)
-        return np.array(path_errors)
+        path_errors: List[np.ndarray] = []
+        for self_path in self.predicted_paths:
+            for other_path in other.predicted_paths:
+                if self_path is None or other_path is None:
+                    continue
+                path_errors.append(self_path.get_path_error(other_path))
+        return np.stack(path_errors)
 
     def get_area_bev(self) -> float:
         """Get area of object BEV.

perception_eval/perception_eval/common/state.py
@@ -18,6 +18,7 @@ from typing import List
 from typing import Optional
 from typing import Tuple
 from typing import Union
+from typing import Generator
 
 import numpy as np
 from perception_eval.common.shape import Shape
@@ -146,27 +147,6 @@ class ObjectPath:
         self.states: List[ObjectState] = states
         self.confidence: float = confidence
 
-    def __getitem__(self, idx: int) -> Union[ObjectState, List[ObjectState]]:
-        """Returns Nth state.
-
-        Args:
-            idx (int): Index.
-
-        Returns:
-            Union[ObjectState, List[ObjectState]]
-        """
-        return self.states[idx]
-
-    def __iter__(self) -> ObjectPath:
-        self.__n = 0
-        return self
-
-    def __next__(self):
-        if self.__n < len(self):
-            self.__n += 1
-            return self[self.__n - 1]
-        raise StopIteration
-
     def __len__(self) -> int:
         """Returns length of states.
 
@@ -174,6 +154,20 @@ class ObjectPath:
             int: length of states.
         """
         return len(self.states)
+    
+    def get_path_error(self, other: ObjectPath) -> np.ndarray:
+        """Return the displacement error at each waypoint of the path.
+
+        Args:
+            other (ObjectPath): Other path.
+
+        Returns:
+            np.ndarray: Displacement error array in the shape of (T, 3).
+        """
+        min_length = min(len(self), len(other))
+        self_states = self.states[:min_length]
+        other_states = other.states[:min_length]
+        return np.array([self_s.get_position_error(other_s) for self_s, other_s in zip(self_states, other_states)]).reshape(-1, 3)
 
 
 def set_object_states(