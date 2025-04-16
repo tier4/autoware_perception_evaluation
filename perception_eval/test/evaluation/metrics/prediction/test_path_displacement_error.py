0d97b63afe77b70e09dc2ad6919897127ffb5f0b
ktro2828 <kotaro.uetake@tier4.jp>
2025-04-16 19:58:27 +0900

    test: update unit tests for path displacement error
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- perception_eval/test/evaluation/metrics/prediction/test_path_displacement_error.py
+++ perception_eval/test/evaluation/metrics/prediction/test_path_displacement_error.py
@@ -14,6 +14,7 @@
 
 from __future__ import annotations
 
+import math
 from test.util.dummy_object import make_dummy_data
 from typing import List
 from typing import Tuple
@@ -22,6 +23,8 @@ from perception_eval.common.evaluation_task import EvaluationTask
 from perception_eval.common.label import AutowareLabel
 from perception_eval.common.label import LabelType
 from perception_eval.common.schema import FrameID
+from perception_eval.common.transform import HomogeneousMatrix
+from perception_eval.common.transform import TransformDict
 from perception_eval.evaluation import DynamicObjectWithPerceptionResult
 from perception_eval.evaluation.metrics.prediction.path_displacement_error import PathDisplacementError
 from perception_eval.evaluation.result.object_result import get_object_results
@@ -30,12 +33,16 @@ import pytest
 
 @pytest.fixture(scope="module")
 def setup() -> Tuple[List[DynamicObjectWithPerceptionResult], int, List[LabelType]]:
-    estimations, ground_truths = make_dummy_data(frame_id=FrameID.MAP)
+    estimations, ground_truths = make_dummy_data(frame_id=FrameID.MAP, use_unique_id=False)
+    transforms = TransformDict(
+        HomogeneousMatrix(position=(0, 0, 0), rotation=(1, 0, 0, 0), src=FrameID.BASE_LINK, dst=FrameID.MAP)
+    )
     return (
         get_object_results(
             evaluation_task=EvaluationTask.PREDICTION,
             estimated_objects=estimations,
             ground_truth_objects=ground_truths,
+            transforms=transforms,
         ),
         len(ground_truths),
         [AutowareLabel.CAR, AutowareLabel.PEDESTRIAN, AutowareLabel.BICYCLE],
@@ -49,7 +56,9 @@ def test_all_path_displacement_error(setup) -> None:
         num_ground_truth=num_ground_truth,
         target_labels=target_labels,
     )
-    print(displacement_err.ade)
+    assert math.isclose(displacement_err.ade, 1.31175, rel_tol=1e-3)
+    assert math.isclose(displacement_err.fde, 1.96204, rel_tol=1e-3)
+    assert math.isclose(displacement_err.miss_rate, 0.333, rel_tol=1e-3)
 
 
 def test_min_path_displacement_error(setup) -> None:
@@ -60,7 +69,9 @@ def test_min_path_displacement_error(setup) -> None:
         target_labels=target_labels,
         kernel="min",
     )
-    print(displacement_err.ade)
+    assert math.isclose(displacement_err.ade, 0.0, rel_tol=1e-3)
+    assert math.isclose(displacement_err.fde, 0.0, rel_tol=1e-3)
+    assert math.isclose(displacement_err.miss_rate, 0.0, rel_tol=1e-3)
 
 
 def test_max_path_displacement_error(setup) -> None:
@@ -71,10 +82,12 @@ def test_max_path_displacement_error(setup) -> None:
         target_labels=target_labels,
         kernel="max",
     )
-    print(displacement_err.ade)
+    assert math.isclose(displacement_err.ade, 2.624, rel_tol=1e-3)
+    assert math.isclose(displacement_err.fde, 3.924, rel_tol=1e-3)
+    assert math.isclose(displacement_err.miss_rate, 0.667, rel_tol=1e-3)
 
 
-def test_highest_path_displacement_error(object_results) -> None:
+def test_highest_path_displacement_error(setup) -> None:
     object_results, num_ground_truth, target_labels = setup
     displacement_err = PathDisplacementError(
         object_results=object_results,
@@ -82,4 +95,6 @@ def test_highest_path_displacement_error(object_results) -> None:
         target_labels=target_labels,
         kernel="highest",
     )
-    print(displacement_err.ade)
+    assert math.isclose(displacement_err.ade, 2.624, rel_tol=1e-3)
+    assert math.isclose(displacement_err.fde, 3.924, rel_tol=1e-3)
+    assert math.isclose(displacement_err.miss_rate, 0.667, rel_tol=1e-3)
