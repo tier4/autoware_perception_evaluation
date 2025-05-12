6da13ff9889c57b99442a17124b278352f3a1888
vividf <yihsiang.fang@tier4.jp>
2025-05-13 00:39:34 +0900

    refactor: test map
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- perception_eval/perception_eval/evaluation/metrics/metrics.py
+++ perception_eval/perception_eval/evaluation/metrics/metrics.py
@@ -12,11 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from collections import defaultdict
-from itertools import chain
 from typing import Dict
 from typing import List
-from typing import Tuple
 from typing import Union
 
 from perception_eval.common.label import LabelType
@@ -27,6 +24,7 @@ from perception_eval.evaluation.result.object_result import DynamicObjectWithPer
 from .classification import ClassificationMetricsScore
 from .detection import Map
 from .metrics_score_config import MetricsScoreConfig
+from .metrics_utils import flatten_and_group_object_results_by_match_config
 from .tracking import TrackingMetricsScore
 
 
@@ -122,46 +120,6 @@ class MetricsScore:
     def num_ground_truth(self) -> int:
         return self.__num_gt
 
-    def flatten_and_group_object_results_by_match_config(
-        self,
-        object_results: Union[
-            Dict[LabelType, Dict[MatchingConfig, List[DynamicObjectWithPerceptionResult]]],
-            Dict[LabelType, Dict[MatchingConfig, List[List[DynamicObjectWithPerceptionResult]]]],
-        ],
-    ) -> Dict[MatchingConfig, Dict[LabelType, List[DynamicObjectWithPerceptionResult]]]:
-        """
-        Flatten nested detection results and regroup them by matching configuration and label.
-
-        This method supports both single-frame and multi-frame object results.
-        In the case of multi-frame results (i.e., lists of lists), the method flattens the
-        nested lists into a single list per (label, MatchingConfig) pair.
-        It then reorganizes the data into a structure grouped first by MatchingConfig
-        and then by LabelType, which simplifies downstream metric computations.
-
-        Args:
-            object_results: A dictionary where the first key is a LabelType, the second key is a MatchingConfig,
-                and the value is either a list of DynamicObjectWithPerceptionResult (for single-frame input) or
-                a list of lists of them (for multi-frame input).
-
-        Returns:
-            A dictionary where each key is a MatchingConfig, and each value is another dictionary
-            mapping LabelType to a flattened list of DynamicObjectWithPerceptionResult.
-        """
-        results_by_match_config: Dict[
-            MatchingConfig, Dict[LabelType, List[DynamicObjectWithPerceptionResult]]
-        ] = defaultdict(dict)
-
-        for label, method_results in object_results.items():
-            for matching_config, result_list in method_results.items():
-                if all(isinstance(r, list) for r in result_list):
-                    result_list_flat = list(chain.from_iterable(result_list))
-                else:
-                    result_list_flat = result_list
-
-                results_by_match_config[matching_config][label] = result_list_flat
-
-        return results_by_match_config
-
     # TODO(vividf): Refactor this function to always accept flattened input (List[DynamicObjectWithPerceptionResult]).
     # Move multi-frame handling and scenario-level aggregation logic to a new function
     # reference: https://github.com/tier4/autoware_perception_evaluation/pull/212#discussion_r2079100088
@@ -198,7 +156,7 @@ class MetricsScore:
 
         # Group by (matching_mode_str, threshold): Dict[LabelType -> List[DynamicObjectWithPerceptionResult]]
         # Example: (center_distance_bev, 0,5): {car: List[DynamicObjectWithPerceptionResult]}
-        results_by_match_config = self.flatten_and_group_object_results_by_match_config(object_results)
+        results_by_match_config = flatten_and_group_object_results_by_match_config(object_results)
 
         # For each (matching_mode, threshold), group labels and compute MAP
         for (matching_mode, threshold), label_results in results_by_match_config.items():
