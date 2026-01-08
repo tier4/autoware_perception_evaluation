c0b6a6a4faf7727918533d18bf37c0cbcf615312
Kok Seang Tan <kseangtan@gmail.com>
2026-01-08 14:39:58 +0900

    Formatting

--- perception_eval/perception_eval/tool/perception_analyzer_base.py
+++ perception_eval/perception_eval/tool/perception_analyzer_base.py
@@ -201,7 +201,7 @@ class PerceptionAnalyzerBase(ABC):
         """
         df = self.df
 
-        mask = np.ones(len(self), dtype=np.bool_)
+        mask = np.ones(len(self), dtype=np.bool8)
         for key, item in kwargs.items():
             if item is None:
                 continue
