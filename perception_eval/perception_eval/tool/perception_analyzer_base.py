7389973fd0ccca0bc3495be69b3ca2e24ee691d5
Kok Seang Tan <kseangtan@gmail.com>
2026-01-08 14:34:28 +0900

    Revert numpy, and restrict word_count version

--- perception_eval/perception_eval/tool/perception_analyzer_base.py
+++ perception_eval/perception_eval/tool/perception_analyzer_base.py
@@ -201,7 +201,7 @@ class PerceptionAnalyzerBase(ABC):
         """
         df = self.df
 
-        mask = np.ones(len(self), dtype=np.bool)
+        mask = np.ones(len(self), dtype=np.bool_)
         for key, item in kwargs.items():
             if item is None:
                 continue
