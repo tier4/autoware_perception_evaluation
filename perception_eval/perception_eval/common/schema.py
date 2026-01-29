5e66d9d2ae9636b3f116fead254177ea0bb201aa
Masato Saeki <78376491+MasatoSaeki@users.noreply.github.com>
2026-01-29 18:01:44 +0900

    feat(perception): support the new camera `cam_front_*wide*` (#250)
    * feat(perception): support
    
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>
    
    * add other cameras
    
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>
    
    ---------
    
    Signed-off-by: MasatoSaeki <masato.saeki@tier4.jp>

--- perception_eval/perception_eval/common/schema.py
+++ perception_eval/perception_eval/common/schema.py
@@ -40,8 +40,11 @@ class FrameID(Enum):
     # 2D
     CAM_FRONT = "cam_front"
     CAM_FRONT_RIGHT = "cam_front_right"
+    CAM_FRONT_RIGHT_WIDE = "cam_front_right_wide"
     CAM_FRONT_LEFT = "cam_front_left"
+    CAM_FRONT_LEFT_WIDE = "cam_front_left_wide"
     CAM_FRONT_LOWER = "cam_front_lower"
+    CAM_FRONT_WIDE = "cam_front_wide"
     CAM_BACK = "cam_back"
     CAM_BACK_LEFT = "cam_back_left"
     CAM_BACK_RIGHT = "cam_back_right"
